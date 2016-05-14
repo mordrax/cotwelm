a9eccbd2664f98ac44909b8ef35ced3fdbfee49a
Joseph Ni <joseph.ni@gmail.com>
2016-05-14 18:58:43 +1000

    adding build stuff

---
+++ build.sh
@@ -0,0 +1,4 @@
+
+elm make main.elm --output index.html
+echo "do some str replace"
+
---
+++ main.elm
@@ -0,0 +1,41 @@
+import Html exposing (..)
+import Html.App
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+
+main = Html.App.beginnerProgram
+  { model = 0
+  , update = update
+  , view = view
+  }
+
+type alias Model = Int
+type Action = Inc | Dec
+
+update: Action -> Model -> Model
+update action model =
+  case action of
+    Inc -> model + 1
+    Dec -> model - 1
+
+view: Model -> Html Action
+view model =
+  div [class "ui middle aligned center aligned grid fullscreen"] [
+    div [class "ui one column"] [
+      div [class "ui column"] [
+        img [src "public/assets/landing_cotw1.jpg"] []
+      ]
+    ],
+
+    div [class "ui column"] [
+      img [src "public/assets/landing_cotw2.jpg"] []
+    ],
+
+    div [class "ui column"] [
+      div [class "ui buttons"] [
+        button [class "ui button primary", onClick Inc] [text "New Game"],
+        button [class "ui button", onClick Inc] [text "Load Game"],
+        button [class "ui button", onClick Inc] [text "Overview"]
+      ]
+    ]
+  ]
