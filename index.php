6e514ab6e03b92d87572753a67b5e37f198acb5b
Joseph Ni <joseph.ni@gmail.com>
2016-05-14 19:01:26 +1000

    added semantic to html

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE HTML>
 <html><head><meta charset="UTF-8"><title>Main</title><style>html,head,body { padding:0; margin:0; }
-body { font-family: calibri, helvetica, arial, sans-serif; }</style><script type="text/javascript">
+body { font-family: calibri, helvetica, arial, sans-serif; }</style>
+<link rel="stylesheet" type="text/css" href="public/css/semantic/semantic.min.css">
+<script type="text/javascript">
 (function() {
 'use strict';
 
