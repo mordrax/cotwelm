50e9fcae7d0c5fff34564f911e11e5ee26895b51
Joseph Ni <joseph.ni@gmail.com>
2016-05-15 01:01:43 +1000

    automating ga

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE HTML>
 <html><head><meta charset="UTF-8"><title>Main</title><style>html,head,body { padding:0; margin:0; }
-body { font-family: calibri, helvetica, arial, sans-serif; }</style>
-<link rel="stylesheet" type="text/css" href="public/css/semantic/semantic.min.css"/>
-<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-66886988-3", "auto");ga("send", "pageview");</script>
-<script type="text/javascript">
+body { font-family: calibri, helvetica, arial, sans-serif; }</style><link rel="stylesheet" type="text/css" href="public/css/semantic/semantic.min.css"/><script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-66886988-3", "auto");ga("send", "pageview");</script><script type="text/javascript">
 (function() {
 'use strict';
 
@@ -7356,6 +7353,4 @@ for (var publicModule in Elm)
 }
 
 }).call(this);
-</script></head><body>
-<?php include_once("analyticstracking.php") ?>
-<script type="text/javascript">Elm.Main.fullscreen()</script></body></html>
+</script></head><body><?php include_once("analyticstracking.php") ?><script type="text/javascript">Elm.Main.fullscreen()</script></body></html>
