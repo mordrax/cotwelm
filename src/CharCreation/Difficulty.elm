1070677f06bf933619af7218c08dd75aab629a1e
Joseph Ni <joseph.ni@gmail.com>
2016-05-17 23:53:53 +1000

    implemented attribute descriptions

--- src/CharCreation/Difficulty.elm
+++ src/CharCreation/Difficulty.elm
@@ -1,35 +1,84 @@
-module CharCreation.Difficulty exposing (view) -- where
+module CharCreation.Difficulty exposing (view)
+
+-- where
 
 import CharCreation.Data exposing (..)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
 
-view: Difficulty -> Html Msg
-view difficulty = 
-  let 
-    activeEasy = if difficulty == Easy then "active" else ""
-    activeIntermediate = if difficulty == Intermediate then "active" else ""
-    activeHard = if difficulty == Hard then "active" else ""
-    activeImpossible = if difficulty == Impossible then "active" else ""
-  in
-    div [class "four ui buttons"] [
-      button [class ("ui icon button " ++ activeEasy), onClick (Difficulty Easy)] [
-        div [] [i [class "huge green circle icon"] []],
-        label [] [text "Easy"]
-      ],
-      button [class ("ui icon button " ++ activeIntermediate), onClick (Difficulty Intermediate)] [
-        div [] [i [class "huge blue square icon"] []],
-        label [] [text "Intermediate"]
-      ],
-      button [class ("ui icon button " ++ activeHard), onClick (Difficulty Hard)] [
-        div [] [i [class "huge black square icon"] []],
-        label [] [text "Hard"]
-      ],
-      button [class ("ui icon button " ++ activeImpossible), onClick (Difficulty Impossible)] [
-        div [] [i [class "huge yellow warning sign icon"] []],
-        label [] [text "Impossible"]
-      ]
-    ]
 
-    
+view : Difficulty -> Html Msg
+view difficulty =
+    let
+        activeEasy =
+            if difficulty == Easy then
+                "active"
+            else
+                ""
+
+        activeIntermediate =
+            if difficulty == Intermediate then
+                "active"
+            else
+                ""
+
+        activeHard =
+            if difficulty == Hard then
+                "active"
+            else
+                ""
+
+        activeImpossible =
+            if difficulty == Impossible then
+                "active"
+            else
+                ""
+    in
+        div [ class "four ui buttons" ]
+            [ easyButton activeEasy
+            , intermediateButton activeIntermediate
+            , hardButton activeHard
+            , impossibleButton activeImpossible
+            ]
+
+
+iconButton : Difficulty -> String -> List (Html Msg) -> Html Msg
+iconButton diff active a =
+    button [ class ("ui icon button " ++ active), onClick (Difficulty diff) ] a
+
+
+easyButton : String -> Html Msg
+easyButton active =
+    iconButton Easy
+        active
+        [ div [] [ i [ class "huge green circle icon" ] [] ]
+        , label [] [ text "Easy" ]
+        ]
+
+
+intermediateButton : String -> Html Msg
+intermediateButton active =
+    iconButton Intermediate
+        active
+        [ div [] [ i [ class "huge blue square icon" ] [] ]
+        , label [] [ text "Intermediate" ]
+        ]
+
+
+hardButton : String -> Html Msg
+hardButton active =
+    iconButton Hard
+        active
+        [ div [] [ i [ class "huge black square icon" ] [] ]
+        , label [] [ text "Hard" ]
+        ]
+
+
+impossibleButton : String -> Html Msg
+impossibleButton active =
+    iconButton Impossible
+        active
+        [ div [] [ i [ class "huge yellow warning sign icon" ] [] ]
+        , label [] [ text "Impossible" ]
+        ]
--- src/SplashView.elm
+++ src/SplashView.elm
@@ -1,42 +1,44 @@
-module SplashView exposing (view, Msg(..)) -- where
+module SplashView exposing (view, Msg(..))
 
-{-| Shows the splash screen
-@docs view
--}
+-- where
+
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
 
-type Msg = 
-  NewGame | 
-  LoadGame | 
-  Overview
+
+type Msg
+    = NewGame
+    | LoadGame
+    | Overview
+
 
 {-| Shows the splash screen
   @doc view
 -}
-view: Html Msg
+view : Html Msg
 view =
-  let 
-    bgStyle = [("backgroundColor", "black")]
-  in
-  div [class "ui middle aligned center aligned grid fullscreen",
-       style bgStyle] [
-    div [class "ui one column"] [
-      div [class "ui column"] [
-        img [src "/public/assets/landing_cotw1.jpg"] []
-      ],
-
-      div [class "ui column"] [
-        img [src "/public/assets/landing_cotw2.jpg"] []
-      ],
-
-      div [class "ui column"] [
-        div [class "ui buttons"] [
-          button [class "ui button primary", onClick NewGame] [text "New Game"],
-          button [class "ui button", onClick LoadGame] [text "Load Game"],
-          button [class "ui button", onClick Overview] [text "Overview"]
-        ]
-      ]
-    ]
-  ]
+    let
+        bgStyle =
+            [ ( "backgroundColor", "black" ) ]
+    in
+        div
+            [ class "ui middle aligned center aligned grid fullscreen"
+            , style bgStyle
+            ]
+            [ div [ class "ui one column" ]
+                [ div [ class "ui column" ]
+                    [ img [ src "/public/assets/landing_cotw1.jpg" ] []
+                    ]
+                , div [ class "ui column" ]
+                    [ img [ src "/public/assets/landing_cotw2.jpg" ] []
+                    ]
+                , div [ class "ui column" ]
+                    [ div [ class "ui buttons" ]
+                        [ button [ class "ui button primary", onClick NewGame ] [ text "New Game" ]
+                        , button [ class "ui button", onClick LoadGame ] [ text "Load Game" ]
+                        , button [ class "ui button", onClick Overview ] [ text "Overview" ]
+                        ]
+                    ]
+                ]
+            ]
