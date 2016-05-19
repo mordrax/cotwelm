e0b354b3997a56d5c30c2b58f72f13755bd09ee3
Joseph Ni <joseph.ni@gmail.com>
2016-05-19 22:56:34 +1000

    update index.php

--- dist/index.php
+++ dist/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE HTML>
-<html><head><meta charset="UTF-8">
-  <title>Castle of the Winds</title>
+<html>
+  <head><meta charset="UTF-8">
+    <title>Castle of the Winds</title>
 
-  <style>html,head,body { padding:0; margin:0; }body { font-family: calibri, helvetica, arial, sans-serif; }</style>
+    <style>html,head,body { padding:0; margin:0; }body { font-family: calibri, helvetica, arial, sans-serif; }</style>
 
-  <link rel="stylesheet" type="text/css" href="css/sui/semantic.min.css"/>
-  <link rel="stylesheet" type="text/css" href="css/tiles.css">
-  <link rel="stylesheet" type="text/css" href="css/buildings.css">
-  <link rel="stylesheet" type="text/css" href="css/items.css">
-  <link rel="stylesheet" type="text/css" href="css/monsters.css">
+    <link rel="stylesheet" type="text/css" href="css/sui/sui.min.import.css"/>
+    <link rel="stylesheet" type="text/css" href="css/tiles.css">
+    <link rel="stylesheet" type="text/css" href="css/buildings.css">
+    <link rel="stylesheet" type="text/css" href="css/items.css">
+    <link rel="stylesheet" type="text/css" href="css/monsters.css">
 
-  <script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-66886988-3", "auto");ga("send", "pageview");</script>
-  <script type="text/javascript" src="js/cotw.js"></script>
-</head>
+    <script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-66886988-3", "auto");ga("send", "pageview");</script>
+    <script type="text/javascript" src="js/cotw.js"></script>
+  </head>
+
   <body><?php include_once("analyticstracking.php") ?>
     <script type="text/javascript">Elm.Main.fullscreen()</script>
   </body>
