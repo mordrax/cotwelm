1cf1bceab8fadfb5a5fb74f6e6f0583e6ea73f22
Joseph Ni <joseph.ni@gmail.com>
2016-06-01 22:03:14 +1000

    added container doco WIP

--- src/Container.elm
+++ src/Container.elm
@@ -1,5 +1,13 @@
 module Container exposing (Container, new)
 
+{-| A container holds Items with a Mass up to a certain amount. This amount is specified by the weight and bulk capacity of the container.
+You can add/remove Items and look through a list of them.
+
+#
+
+Items can also be containers, so containers can hold containers.
+-}
+
 import Mass exposing (..)
 
 
