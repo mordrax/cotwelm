1b41c3d85f1364aee3c64666ee535c073f644415
Joseph Ni <joseph.ni@gmail.com>
2016-06-05 16:09:09 +1000

    moving view code out of Equipment, making all equipment draggable

--- src/Equipment.elm
+++ src/Equipment.elm
@@ -1,15 +1,16 @@
 module Equipment
     exposing
-        ( EquipmentSlot
+        ( EquipmentSlot(..)
         , Msg
         , Equipment
-        , getPack
+        , get
         , init
-        , viewEquipment
         )
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+{-| Manages equipment slots and any items that are equipped in those slots.
+Does not render equipment but will provide a API to retrieve them.
+-}
+
 import GameData.Item as Item exposing (..)
 
 
@@ -26,32 +27,32 @@ type alias Model =
     , pack : Maybe Item
     , neckwear : Maybe Item
     , overgarment : Maybe Item
-    , leftring : Maybe Item
-    , rightring : Maybe Item
+    , leftRing : Maybe Item
+    , rightRing : Maybe Item
     , boots : Maybe Item
     }
 
 
 type Equipment
-    = EM Model
+    = EquipmentModel Model
 
 
 type EquipmentSlot
-    = WeaponSlot
-    | FreehandSlot
-    | ArmourSlot
-    | ShieldSlot
-    | HelmetSlot
-    | BracersSlot
-    | GauntletsSlot
-    | BeltSlot
-    | PurseSlot
-    | PackSlot
-    | NeckwearSlot
-    | OvergarmentSlot
-    | LeftRingSlot
-    | RightRingSlot
-    | BootsSlot
+    = Weapon
+    | Freehand
+    | Armour
+    | Shield
+    | Helmet
+    | Bracers
+    | Gauntlets
+    | Belt
+    | Purse
+    | Pack
+    | Neckwear
+    | Overgarment
+    | LeftRing
+    | RightRing
+    | Boots
 
 
 type Msg
@@ -67,26 +68,26 @@ init =
             newPack MediumPack Normal Identified
 
         ths =
-            new (Weapon TwoHandedSword) Normal Identified
+            new (Item.Weapon TwoHandedSword) Normal Identified
 
         pack' =
             Item.addToPack ths pack
     in
-        EM
-            { weapon = Just (new (Weapon Dagger) Normal Identified)
+        EquipmentModel
+            { weapon = Just (new (Item.Weapon Dagger) Normal Identified)
             , freehand = Nothing
-            , armour = Just (new (Armour LeatherArmour) Normal Identified)
-            , shield = Just (new (Shield SmallWoodenShield) Normal Identified)
-            , helmet = Just (new (Helmet LeatherHelmet) Normal Identified)
-            , bracers = Just (new (Bracers NormalBracers) Normal Identified)
-            , gauntlets = Just (new (Gauntlets NormalGauntlets) Normal Identified)
-            , belt = Just (new (Belt TwoSlotBelt) Normal Identified)
+            , armour = Just (new (Item.Armour LeatherArmour) Normal Identified)
+            , shield = Just (new (Item.Shield SmallWoodenShield) Normal Identified)
+            , helmet = Just (new (Item.Helmet LeatherHelmet) Normal Identified)
+            , bracers = Just (new (Item.Bracers NormalBracers) Normal Identified)
+            , gauntlets = Just (new (Item.Gauntlets NormalGauntlets) Normal Identified)
+            , belt = Just (new (Item.Belt TwoSlotBelt) Normal Identified)
             , purse = Nothing
             , pack = Just (ItemPack pack')
             , neckwear = Nothing
             , overgarment = Nothing
-            , leftring = Nothing
-            , rightring = Nothing
+            , leftRing = Nothing
+            , rightRing = Nothing
             , boots = Nothing
             }
 
@@ -101,47 +102,50 @@ equip slot item model =
             ( model, Ok )
 
 
-getPack : Equipment -> Maybe Item
-getPack (EM model) =
-    model.pack
+get : EquipmentSlot -> Equipment -> Maybe Item
+get slot (EquipmentModel model) =
+    case slot of
+        Weapon ->
+            model.weapon
 
+        Freehand ->
+            model.freehand
 
-equipmentSlotStyle : Html.Attribute msg
-equipmentSlotStyle =
-    style [ ( "border", "1px Solid Black" ) ]
+        Armour ->
+            model.armour
 
+        Shield ->
+            model.shield
 
-viewEquipment : Equipment -> Html msg
-viewEquipment (EM model) =
-    div []
-        [ viewIfItem model.weapon
-        , viewIfItem model.freehand
-        , viewIfItem model.armour
-        , viewIfItem model.shield
-        , viewIfItem model.helmet
-        , viewIfItem model.bracers
-        , viewIfItem model.gauntlets
-        , viewIfItem model.belt
-        , viewIfItem model.purse
-        , viewIfItem model.pack
-        , viewIfItem model.neckwear
-        , viewIfItem model.overgarment
-        , viewIfItem model.leftring
-        , viewIfItem model.rightring
-        , viewIfItem model.boots
-        ]
+        Helmet ->
+            model.helmet
 
+        Bracers ->
+            model.bracers
 
-viewIfItem : Maybe Item -> Html msg
-viewIfItem maybeItem =
-    let
-        slotCss =
-            class "three wide column equipmentSlot"
-    in
-        case maybeItem of
-            Just item ->
-                div [ slotCss ]
-                    [ Item.view item ]
+        Gauntlets ->
+            model.gauntlets
 
-            Nothing ->
-                div [ slotCss ] [ text "Empty" ]
+        Belt ->
+            model.belt
+
+        Purse ->
+            model.purse
+
+        Pack ->
+            model.pack
+
+        Neckwear ->
+            model.neckwear
+
+        Overgarment ->
+            model.overgarment
+
+        LeftRing ->
+            model.leftRing
+
+        RightRing ->
+            model.rightRing
+
+        Boots ->
+            model.boots
