7015f70b0031e6d142e6c5e566e9389dca0d2871
Joseph Ni <joseph.ni@gmail.com>
2016-06-14 00:18:44 +1000

    refactoring out drag/drop into DragDrop.elm WIP

---
+++ src/DragDrop.elm
@@ -0,0 +1,196 @@
+module DragDrop
+    exposing
+        ( DragDrop
+        , Drag(..)
+        , Drop(..)
+        , DragDropMsg(..)
+        , new
+        , view
+        , update
+        , draggable
+        , droppable
+        )
+
+import Mouse exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as JD exposing (..)
+
+
+type DragDrop source target
+    = DragDropModel (Model source target)
+
+
+type alias Model source target =
+    { dragSource : Drag source
+    , dropTarget : Drop target
+    , position : Position
+    , dragging : Maybe (Dragging source target)
+    }
+
+
+type Drag a
+    = DragSource a
+    | NoDrag
+
+
+type Drop a
+    = DropTarget a
+    | NoDrop
+
+
+type alias Dragging source target =
+    { start : Position
+    , current : Position
+    , html : Html (DragDropMsg source target)
+    }
+
+
+type DragDropMsg source target
+    = Start (Drag source) (Html (DragDropMsg source target)) Position
+    | At (Drag source) (Html (DragDropMsg source target)) Position
+    | End Position
+    | MouseOver (Drop target)
+    | MouseLeave
+
+
+new : source -> target -> DragDrop source target
+new source target =
+    DragDropModel
+        { dragSource = NoDrag
+        , dropTarget = NoDrop
+        , position = Position 0 0
+        , dragging = Nothing
+        }
+
+
+update : DragDropMsg s t -> DragDrop s t -> DragDrop s t
+update msg (DragDropModel model) =
+    let
+        startDrag =
+            \source html pos -> Model source model.dropTarget pos (Just (Dragging pos pos html))
+
+        atDrag =
+            \source html pos -> Model source model.dropTarget model.position (Maybe.map (\{ start } -> (Dragging start pos html)) model.dragging)
+    in
+        case msg of
+            Start source html pos ->
+                (DragDropModel (startDrag source html pos))
+
+            At source html pos ->
+                DragDropModel <| atDrag source html pos
+
+            -- on drag end, check if it's over a droppable container
+            End _ ->
+                Debug.crash "TODO: DragDrop.elm handleMouseUp"
+
+            --(handleMouseUp model)
+            MouseOver dropTarget ->
+                DragDropModel { model | dropTarget = dropTarget }
+
+            MouseLeave ->
+                DragDropModel { model | dropTarget = NoDrop }
+
+
+view : DragDrop s t -> Html (DragDropMsg s t)
+view (DragDropModel ({ dragSource, position, dragging } as model)) =
+    let
+        px =
+            \x -> toString x ++ "px"
+
+        newPos =
+            getDisplacement model
+
+        positionStyle =
+            style
+                [ ( "top", px newPos.y )
+                , ( "left", px newPos.x )
+                , ( "position", "absolute" )
+                , ( "cursor", "move" )
+                ]
+
+        pointerEventStyle =
+            style [ ( "pointer-events", "none" ) ]
+    in
+        case model.dragging of
+            Just { start, current, html } ->
+                div [ positionStyle, pointerEventStyle ] [ html ]
+
+            _ ->
+                div [] []
+
+
+{-| DnDModel tracks where the mouse starts and where it currently is to get the absolute
+movement from when mouse down happens. This is the actual drag distance.
+-}
+getDisplacement : Model s t -> Position
+getDisplacement { dragSource, dropTarget, position, dragging } =
+    case dragging of
+        Nothing ->
+            position
+
+        Just { start, current } ->
+            Position (position.x + current.x - start.x)
+                (position.y + current.y - start.y)
+
+
+{-| Takes a html snippet that can be dragged and drag it around the screen.
+The snippet is attached to a arbitrary Drag source which is given to the Drop target when dropped.
+-}
+draggable : Html (DragDropMsg s t) -> s -> DragDrop s t -> Html (DragDropMsg s t)
+draggable draggableHtml source (DragDropModel model) =
+    let
+        dragSource =
+            DragSource source
+
+        onMouseDown =
+            onWithOptions "mousedown"
+                { stopPropagation = True, preventDefault = True }
+                (JD.map (Start dragSource draggableHtml) Mouse.position)
+
+        pointerEventStyle =
+            case model.dragging of
+                Just _ ->
+                    style [ ( "pointer-events", "none" ) ]
+
+                Nothing ->
+                    style [ ( "pointer-events", "inherit" ) ]
+    in
+        div [ onMouseDown, pointerEventStyle ] [ draggableHtml ]
+
+
+droppable : t -> DragDrop s t -> Html (DragDropMsg s t) -> Html (DragDropMsg s t)
+droppable target (DragDropModel model) html =
+    let
+        dropTarget =
+            DropTarget target
+
+        borderStyle =
+            if model.dropTarget == dropTarget then
+                style [ ( "border", "1px solid" ) ]
+            else
+                style [ ( "border", "none" ) ]
+
+        mouseOverStyle =
+            on "mouseover" (JD.succeed <| MouseOver dropTarget)
+
+        mouseLeaveStyle =
+            onMouseLeave MouseLeave
+    in
+        div [ mouseOverStyle, mouseLeaveStyle, borderStyle ] [ html ]
+
+
+subscriptions : DragDrop s t -> List (Sub (DragDropMsg s t))
+subscriptions (DragDropModel model) =
+    case model.dragSource of
+        NoDrag ->
+            [ Sub.none ]
+
+        source ->
+            case model.dragging of
+                Just { start, current, html } ->
+                    [ Mouse.moves (At source html), Mouse.ups End ]
+
+                _ ->
+                    [ Sub.none ]
