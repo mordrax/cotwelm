7910f309bd7420cf173cf1a5090ca0f9ecc5b0db
Joseph Ni <joseph.ni@gmail.com>
2016-06-25 20:43:17 +1000

    monsters WIP

---
+++ src/Monster/List.elm
@@ -0,0 +1,166 @@
+level: 100 name: "Abyss Fiend"                 Special: "Summons Spiked Devil or Abyss Fiend"
+level: 70  name: "Abyss Wraith"                Special: "Drains Intelligence and Mana Permanently"
+level: 55  name: "Adult Blue Dragon"           Special: "Immune to Lightning, Breathes Lightning"
+level: 55  name: "Adult Green Dragon"          Special: "Immune to Poison, Breathes Poison Gas"
+level: 60  name: "Adult Red Dragon"            Special: "Immune to Fire, Breathes Fire"
+level: 55  name: "Adult White Dragon"          Special: "Immune to Cold, Breathes Ice"
+level: 55  name: "Air Elemental"               Special: " "
+level: 110 name: "Ancient Blue Dragon"         Special: "Immune to Lightning, Breathes Lightning"
+level: 105 name: "Ancient Green Dragon"        Special: "Immune to Poison, Breathes Poison Gas"
+level: 120 name: "Ancient Red Dragon"          Special: "Immune to Fire, Breathes Fire"
+level: 105 name: "AncientWhite Dragon"         Special: "Immune to Cold, Breathes Ice"
+level: 25  name: "Animated Bronze Statue"      Special: " "
+level: 35  name: "Animated Iron Statue"        Special: " "
+level: 52  name: "Animated Marble Statue"      Special: " "
+level: 17  name: "Animated Wooden Statue"      Special: " "
+level: 10  name: "Bandit"                      Special: "Arrow"
+level: 40  name: "Barrow Wight"                Special: "Drains Strength, Constitution, and Dexterity Permanently"
+level: 30  name: "Berserker"                   Special: " "
+level: 40  name: "Bear-Man"                    Special: " "
+level: 17  name: "Brown Bear"                  Special: " "
+level: 50  name: "Bull-Man"                    Special: " "
+level: 16  name: "Carrion Creeper"             Special: " "
+level: 50  name: "Castle Wight"                Special: "Drains Strength, Constitution, and Dexterity Permanently"
+level: 27  name: "Cave Bear"                   Special: " "
+level: 50  name: "Dark Wraith"                 Special: "Drains Intelligence and Mana Permanently"
+level: 40  name: "Dust Elemental"              Special: " "
+level: 65  name: "Earth Elemental"             Special: " "
+level: 20  name: "Eerie Ghost"                 Special: " "
+level: 25  name: "Evil Warrior"                Special: "Arrow"
+level: 60  name: "Fire Elemental"              Special: " "
+level: 75  name: "Fire Giant"                  Special: "Throws Stones"
+level: 60  name: "Frost Giant"                 Special: "Throws Iceballs"
+level: 14  name: "Gelantinous Glob"            Special: "Immune to Cold, Lightning"
+level: 2   name: "Giant Bat"                   Special: " "
+level: 1   name: "Giant Rat"                   Special: " "
+level: 7   name: "Giant Red Ant"               Special: " "
+level: 11  name: "Giant Scorpion"              Special: "Poison"
+level: 10  name: "Giant Trapdoor Spider"       Special: " "
+level: 1   name: "Goblin"                      Special: " "
+level: 6   name: "Goblin Fighter"              Special: " "
+level: 11  name: "Gray Wolf"                   Special: " "
+level: 20  name: "Gruesome Troll"              Special: " "
+level: 40  name: "Hill Giant"                  Special: "Throws Stones"
+level: 2   name: "Hobgoblin"                   Special: " "
+level: 80  name: "Horned Devil"                Special: "Summons Horned Devil"
+level: 10  name: "Huge Lizard"                 Special: " "
+level: 16  name: "Huge Ogre"                   Special: " "
+level: 85  name: "Ice Devil"                   Special: "Summons Ice Devil"
+level: 50  name: "Ice Elemental"               Special: " "
+level: 2   name: "Kobold"                      Special: " "
+level: 3   name: "Large Snake"                 Special: " "
+level: 55  name: "Magma Elemental"             Special: " "
+level: 19  name: "Manticore"                   Special: "Needles"
+level: 65  name: "Necromancer"                 Special: "Casts Bolt Spells, Slow, Summon Monster, Phase Door, Teleport"
+level: 75  name: "Old Blue Dragon"             Special: "Immune to Lightning, Breathes Lightning"
+level: 75  name: "Old Green Dragon"            Special: "Immune to Poison, Breathes Poison Gas"
+level: 80  name: "Old Red Dragon"              Special: "Immune to Fire, Breathes Fire"
+level: 75  name: "Old White Dragon"            Special: "Immune to Cold, Breathes Ice"
+level: 37  name: "Pale Wraith"                 Special: "Drains Intelligence and Mana Permanently"
+level: 10  name: "Rat-Man"                     Special: " "
+level: 80  name: "Rungnir"                     Special: "Throws stones"
+level: 16  name: "Shadow"                      Special: " "
+level: 3   name: "Skeleton"                    Special: " "
+level: 10  name: "Slime"                       Special: "Immune to Weapons"
+level: 15  name: "Smirking Sneak Thief"        Special: "Steals from Belt and Purse, Teleports"
+level: 50  name: "Spectre"                     Special: " "
+level: 60  name: "Spiked Devil"                Special: "Summons Spiked Devil"
+level: 55  name: "Stone Giant"                 Special: "Throws Stones"
+level: 344 name: "Sultur"                      Special: "Casts Fire, Lighting, and Wind Spells"
+level: 100 name: "Thiassa"                     Special: "Throws Stones"
+level: 90  name: "Thrym"                       Special: "Throws Iceballs"
+level: 35  name: "Tunnel Wight"                Special: "Drains Strength, Constitution, and Dexterity Permanently"
+level: 55  name: "Two Headed Giant"            Special: " "
+level: 70  name: "Utgardhalok"                 Special: "Throws Boulders"
+level: 65  name: "Vampire"                     Special: "Drains HP Permanently"
+level: 95  name: "Very Old Blue Dragon"        Special: "Immune to Lightning, Breathes Lightning"
+level: 90  name: "Very Old Green Dragon"       Special: "Immune to Poison, Breathes Poison Gas"
+level: 100 name: "Very Old Red Dragon"         Special: "Immune to Fire, Breathes Fire"
+level: 90  name: "Very Old White Dragon"       Special: "Immune to Cold, Breathes Ice"
+level: 5   name: "Viper"                       Special: "Poison"
+level: 7   name: "Walking Corpse"              Special: " "
+level: 60  name: "Water Elemental"             Special: " "
+level: 28  name: "White Wolf"                  Special: " "
+level: 3   name: "Wild Dog"                    Special: " "
+level: 45  name: "Wizard"                      Special: "Casts Bolt Spells, Slow, Summon Monster, Phase Door, Teleport"
+level: 25  name: "Wolf-Man"                    Special: " "
+level: 35  name: "Young Adult Blue Dragon"     Special: "Immune to Lightning, Breathes Lightning"
+level: 35  name: "Young Adult Green Dragon"    Special: "Immune to Poison, Breathes Poison Gas"
+level: 40  name: "Young Adult Red Dragon"      Special: "Immune to Fire, Breathes Fire"
+level: 35  name: "Young Adult White Dragon"    Special: "Immune to Cold, Breathes Ice"
+level: 20  name: "Young Blue Dragon"           Special: "Immune to Lightning, Breathes Lightning"
+level: 18  name: "Young Green Dragon"          Special: "Immune to Poison, Breathes Poison Gas"
+level: 20  name: "Young Red Dragon"            Special: "Immune to Fire, Breathes Fire"
+level: 18  name: "Young White Dragon"          Special: "Immune to Cold, Breathes Ice"
+
+
+.maleHero
+.femaleHero
+.kobold
+.giantRat
+.largeSnake
+.giantAnt
+.wildDog
+.skeleton
+.giantTrapdoorSpider
+.giantBat
+.carrionCreeper
+.giantScorpion
+.greenSlime
+.viper
+.ogre
+.walkingCorpse
+.hugeLizard
+.goblin
+.hobgoblin
+.spectre
+.thief
+.wolf
+.direWolf
+.brownBear
+.caveBear
+.gelatinousGlob
+.troll
+.manticore
+.bronzeGolem
+.ironGolem
+.diamondGolem
+.woodenGolem
+.bandit
+.warrior
+.wizard
+.necromancer
+.wight
+.wraith
+.ghost
+.shadow
+.vampire
+.iceDevil
+.ratMan
+.wolfMan
+.bearMan
+.bullMan
+.spikedDevil
+.hornedDevil
+.abyssFiend
+.windElemental
+.dustElemental
+.fireElemental
+.waterElemental
+.magmaElemental
+.iceElemental
+.earthElemental
+.hillGiant
+.twoHeadGiant
+.frostGiant
+.stoneGiant
+.fireGiant
+.surtur
+.fireGiantKing
+.frostGiantKing
+.hillGiantKing
+.stoneGiantKing
+.redDragon
+.blueDragon
+.whiteDragon
+.greenDragon
