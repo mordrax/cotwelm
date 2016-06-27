1f9a660f1cd20809920fbcc08a997fc8e6d142cd
mordra <joseph.ni@gmail.com>
2016-06-27 23:07:45 +1000

    kobolds and giant rats are invading the farm, but they can't move yet! quick, kill them while you can... but you can't because I haven't implemented combat...doh!

--- src/Monster/Monsters.elm
+++ src/Monster/Monsters.elm
@@ -1,10 +1,11 @@
 module Monster.Monsters exposing (..)
 
 import Monster.Monster as Monster exposing (..)
+import Utils.Vector as Vector exposing (..)
 
 
 init : List Monster
 init =
-    [ Monster.new GiantRat
-    , Monster.new Kobold
+    [ Monster.new GiantRat (Vector.new 10 1)
+    , Monster.new Kobold (Vector.new 1 10)
     ]
