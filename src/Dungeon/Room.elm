09fdd47f634b0f256d8e4ec82605f9ff80d6e566
Joseph Ni <joseph.ni@gmail.com>
2016-07-24 17:22:28 +1000

    updated Dice refactored starting position out of Rooms rooms are random size and positions (min size 3) doors are randomly placed game now calculates window size and viewport on load rather than on resize event

--- src/Dungeon/Room.elm
+++ src/Dungeon/Room.elm
@@ -1,4 +1,11 @@
-module Dungeon.Room exposing (..)
+module Dungeon.Room
+    exposing
+        ( Room
+        , RoomType(..)
+        , Entrance(..)
+        , design
+        , new
+        )
 
 import Utils.Vector as Vector exposing (..)
 import Random exposing (..)
@@ -71,34 +78,34 @@ design (A { doors, walls, floor }) =
 one or more entrances which can be a door, broken door or clear corridor.
 
 -}
-new : RoomType -> Dimension -> Vector -> Random.Seed -> ( Room, Random.Seed )
-new roomType (( w, h ) as dim) (( x, y ) as start) seed =
+new : RoomType -> Dimension -> Random.Seed -> ( Room, Random.Seed )
+new roomType (( w, h ) as dim) seed =
     let
         ( walls, floor ) =
             case roomType of
                 Rectangular ->
-                    rectangular start dim
+                    rectangular dim
 
                 Cross ->
-                    cross start dim
+                    cross dim
 
                 Diamond ->
-                    diamond start dim
+                    diamond dim
 
                 Potion ->
-                    potion start dim
+                    potion dim
 
                 Circular ->
-                    circular start dim
+                    circular dim
 
                 DiagonalSquares ->
-                    diagonalSquares start dim
+                    diagonalSquares dim
 
                 DeadEnd ->
                     -- will generate one tile for the room which is forced
                     -- into an entrance as all rooms must have at least
                     -- one entrance
-                    ( [], [ ( x, y ) ] )
+                    ( [], [ ( 0, 0 ) ] )
 
         ( walls', entrances, seed' ) =
             List.foldl addDoors ( [], [], seed ) walls
@@ -114,11 +121,66 @@ new roomType (( w, h ) as dim) (( x, y ) as start) seed =
         )
 
 
+
+--------------------------------------------------------------
+-- Different room types
+
+
+rectangular : Dimension -> RoomComponents
+rectangular ( w, h ) =
+    let
+        ( xMax, yMax ) =
+            ( w - 1, h - 1 )
+
+        cornerPoints =
+            [ ( 0, 0 ), ( xMax, 0 ), ( 0, yMax ), ( xMax, yMax ) ]
+
+        corners =
+            List.map makeHardWall cornerPoints
+
+        softWall =
+            \x y -> makeSoftWall ( x, y )
+
+        walls =
+            lift2 softWall [1..xMax - 1] [ 0, yMax ]
+                ++ lift2 softWall [ 0, xMax ] [1..yMax - 1]
+
+        floors =
+            List.Extra.lift2 (,) [1..xMax - 1] [1..yMax - 1]
+    in
+        ( walls ++ corners, floors )
+
+
+cross : Dimension -> RoomComponents
+cross ( w, h ) =
+    ( [], [] )
+
+
+diamond : Dimension -> RoomComponents
+diamond ( w, h ) =
+    ( [], [] )
+
+
+potion : Dimension -> RoomComponents
+potion ( w, h ) =
+    ( [], [] )
+
+
+circular : Dimension -> RoomComponents
+circular ( w, h ) =
+    ( [], [] )
+
+
+diagonalSquares : Dimension -> RoomComponents
+diagonalSquares ( w, h ) =
+    ( [], [] )
+
+
 addDoors : ( Vector, Bool ) -> ( List ( Vector, Bool ), List ( Entrance, Vector ), Random.Seed ) -> ( List ( Vector, Bool ), List ( Entrance, Vector ), Random.Seed )
 addDoors (( wallPos, isSoft ) as wall) ( walls, entrances, seed ) =
     let
         ( res, seed' ) =
-            Dice.d 10 seed
+            Dice.rollD 10 seed
 
         addToWall =
             ( wall :: walls, entrances, seed' )
@@ -151,58 +213,3 @@ makeHardWall pos =
 makeSoftWall : Vector -> ( Vector, Bool )
 makeSoftWall pos =
     ( pos, True )
-
-
-
---------------------------------------------------------------
--- Different room types
-
-
-rectangular : Dimension -> Vector -> RoomComponents
-rectangular ( w, h ) ( x, y ) =
-    let
-        ( xMax, yMax ) =
-            ( x + w - 1, y + h - 1 )
-
-        cornerPoints =
-            [ ( x, y ), ( xMax, y ), ( x, yMax ), ( xMax, yMax ) ]
-
-        corners =
-            List.map makeHardWall cornerPoints
-
-        softWall =
-            \x y -> makeSoftWall ( x, y )
-
-        walls =
-            lift2 softWall [x + 1..xMax - 1] [ y, yMax ]
-                ++ lift2 softWall [ x, xMax ] [y + 1..yMax - 1]
-
-        floors =
-            List.Extra.lift2 (,) [x + 1..xMax - 1] [y + 1..yMax - 1]
-    in
-        ( walls ++ corners, floors )
-
-
-cross : Dimension -> Vector -> RoomComponents
-cross ( w, h ) ( x, y ) =
-    ( [], [] )
-
-
-diamond : Dimension -> Vector -> RoomComponents
-diamond ( w, h ) ( x, y ) =
-    ( [], [] )
-
-
-potion : Dimension -> Vector -> RoomComponents
-potion ( w, h ) ( x, y ) =
-    ( [], [] )
-
-
-circular : Dimension -> Vector -> RoomComponents
-circular ( w, h ) ( x, y ) =
-    ( [], [] )
-
-
-diagonalSquares : Dimension -> Vector -> RoomComponents
-diagonalSquares ( w, h ) ( x, y ) =
-    ( [], [] )
