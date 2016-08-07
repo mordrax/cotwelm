b49b89c4a91713bbc466fd22b0c0fa706535a84b
Joseph Ni <joseph.ni@gmail.com>
2016-08-07 22:51:20 +1000

    shuffling cross walls before adding doors to randomly place doors on different walls

--- src/Dungeon/Rooms/Rectangular.elm
+++ src/Dungeon/Rooms/Rectangular.elm
@@ -1,12 +1,10 @@
 module Dungeon.Rooms.Rectangular exposing (generate)
 
-import Array exposing (..)
 import Dice exposing (..)
 import List.Extra exposing (..)
 import Random exposing (..)
-import Random.Extra exposing (..)
-import Random.Array exposing (..)
 import Dungeon.Rooms.Type exposing (..)
+import Dungeon.Rooms.Config as Config exposing (..)
 
 
 generate : Int -> Random.Seed -> ( Room, Random.Seed )
@@ -33,7 +31,7 @@ generate size seed =
             Dice.d 6
 
         ( ( shuffledWalls, nDoors ), seed'' ) =
-            Random.step (Random.pair (shuffle walls) doorGenerator) seed'
+            Random.step (Random.pair (Config.shuffle walls) doorGenerator) seed'
 
         ( newWalls, doors, seed''' ) =
             addDoors nDoors ( shuffledWalls, [], [], seed'' )
@@ -42,14 +40,6 @@ generate size seed =
             List.Extra.lift2 (,) [1..xMax - 1] [1..yMax - 1]
     in
         ( Room doors newWalls floors corners Rectangular ( width, height ), seed''' )
-
-
-shuffle : List a -> Generator (List a)
-shuffle list =
-    list
-        |> Array.fromList
-        |> Random.Array.shuffle
-        |> Random.map Array.toList
 
 
 {-| Continually adds a door to the next wall until no more doors are left or
@@ -70,21 +60,15 @@ addDoors nDoors ( walls, fullWalls, doors, seed ) =
         ( _, [] :: restOfWalls ) ->
             ( List.concat (walls ++ fullWalls), doors, seed )
 
-        ( n, (rock :: wall) :: restOfWalls ) ->
+        ( n, wall :: restOfWalls ) ->
             let
                 randomDoorSampler =
-                    doorSampleWithDefault (rock :: wall) rock
+                    Config.wallSampler wall
 
                 ( door, seed' ) =
                     Random.step randomDoorSampler seed
 
                 wallWithoutDoor =
-                    List.filter (\x -> x /= door) (rock :: wall)
+                    List.filter (\x -> x /= door) wall
             in
                 addDoors (n - 1) ( restOfWalls ++ [ wallWithoutDoor ], fullWalls, ( Door, door ) :: doors, seed' )
-
-
-doorSampleWithDefault : Walls -> Wall -> Generator Wall
-doorSampleWithDefault walls wall =
-    Random.Extra.sample walls
-        |> Random.map (Maybe.withDefault wall)
