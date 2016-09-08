5754be2f0c01a37e2df8ba3fe5fa647024b2f6cc
Joseph Ni <joseph.ni@gmail.com>
2016-09-08 22:46:16 +1000

    refactoring prospector

--- src/Lodash.elm
+++ src/Lodash.elm
@@ -3,6 +3,7 @@ module Lodash exposing (..)
 import Array exposing (..)
 import Random exposing (..)
 import Random.Array exposing (..)
+import Maybe exposing (withDefault)
 
 
 shuffle : List a -> Generator (List a)
@@ -11,6 +12,13 @@ shuffle list =
         |> Array.fromList
         |> Random.Array.shuffle
         |> Random.map Array.toList
+
+
+headWithDefault : x -> List x -> x
+headWithDefault default xs =
+    xs
+        |> List.head
+        |> Maybe.withDefault default
 
 
 without : a -> List a -> List a
