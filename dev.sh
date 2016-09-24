405979aea51892567d85a67f1a674514825036cc
Joseph Ni <joseph.ni@gmail.com>
2016-09-24 11:17:03 +1000

    added a Should library simplified test expressions added testing to dev env

--- dev.sh
+++ dev.sh
@@ -1,7 +1,16 @@
 
+# elm game running on 8008
+
 elm reactor --port 8000 &
 http-server dist -p 8008 &
-sleep 1
+sleep 2
 browser-sync start --proxy "localhost:8000" --port 5000 --files "**/*.elm" &
 browser-sync start --proxy "localhost:8008" --port 5008 --files "dist/js/cotw.js" &
 
+# elm test
+
+cd tests
+elm reactor --port 1234 &
+sleep 2
+browser-sync start --proxy "localhost:1234" --port 1235 --files "**/*.elm" &
+cd ..
