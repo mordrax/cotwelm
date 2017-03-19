7bf6f55bdb00c3d4df9e565cfd37613592436c7e
Joseph Ni <joseph.ni@blake.com.au>
2017-03-19 19:34:58 +1100

    WIP refactoring Round, Match, Types into modules moved Arena to PlayerArena created a MonsterArena for monster vs monster

---
+++ src/Arena/MonsterArena.elm
@@ -0,0 +1,17 @@
+module Arena.MonsterArena exposing (init, view)
+
+import Html exposing (..)
+
+
+type alias MonsterArena =
+    {}
+
+
+init : MonsterArena
+init =
+    {}
+
+
+view : MonsterArena -> Html msg
+view arena =
+    div [] []
---
+++ src/Arena/Types.elm
@@ -0,0 +1,11 @@
+module Arena.Types exposing (..)
+
+import Combat
+
+
+type alias Blue =
+    Combat.Fighter
+
+
+type alias Red =
+    Combat.Fighter
