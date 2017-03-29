4ee61d56f0012907e4c038f9c07faeb253e08d20
Joseph Ni <joseph.ni@blake.com.au>
2017-03-30 00:41:31 +1100

    fov, los WIP refactoring level to use FieldOfView module

--- src/Utils/FieldOfView.elm
+++ src/Utils/FieldOfView.elm
@@ -1,6 +1,6 @@
-module Utils.FieldOfView exposing (fov)
+module Utils.FieldOfView exposing (fov, los)
 
-import Utils.Vector exposing (Vector)
+import Utils.Vector as Vector exposing (Vector)
 import EveryDict exposing (EveryDict)
 import Utils.BresenhamLine as BresenhamLine
 
@@ -10,40 +10,30 @@ type alias Map =
 
 
 fov : Vector -> (Vector -> Bool) -> (Vector -> List Vector) -> List Vector
-fov source isSolid neighbours =
-    fov_ source source isSolid neighbours
+fov source isSeeThrough neighbours =
+    fov_ source source isSeeThrough neighbours
 
 
 fov_ : Vector -> Vector -> (Vector -> Bool) -> (Vector -> List Vector) -> List Vector
-fov_ source current isSolid neighbours =
+fov_ source current isSeeThrough neighbours =
     let
         inSight target =
-            lineOfSight source target isSolid
+            los source target isSeeThrough
 
         inView revealedPosition =
-            fov_ source revealedPosition isSolid neighbours
+            fov_ source revealedPosition isSeeThrough neighbours
     in
         neighbours current
-            |> List.map (inSight >> inView)
+            |> List.filter inSight
+            |> List.map inView
             |> List.concat
 
 
-lineOfSight : Vector -> Vector -> (Vector -> Bool) -> Bool
-lineOfSight a b isSolid ({ map, rooms } as level) =
+los : Vector -> Vector -> (Vector -> Bool) -> Bool
+los a b isSeeThrough =
     let
-        roomLightSource tile =
-            roomAtPosition tile.position rooms
-                |> Maybe.map .lightSource
-                |> Maybe.withDefault Dark
-
-        isSeeThroughOrEitherEndpoints tile =
-            ((not tile.solid) && (tile.type_ /= Tile.Types.DoorClosed) && (roomLightSource tile /= Dark))
-                || (tile.position == a)
-                || (tile.position == b)
-                || (Vector.adjacent a b)
+        isSeeThroughOrEitherEndpoints point =
+            (isSeeThrough point) || (point == a) || (point == b) || Vector.adjacent a b
     in
         BresenhamLine.line a b
-            |> List.map (\point -> Dict.get point map)
-            |> List.map (Maybe.map isSeeThroughOrEitherEndpoints)
-            |> List.map (Maybe.withDefault False)
-            |> List.all identity
+            |> List.all isSeeThroughOrEitherEndpoints
