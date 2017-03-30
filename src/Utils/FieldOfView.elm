f1a68b02ceae49688e080129cbd903e490350496
Joseph Ni <joseph.ni@gmail.com>
2017-03-30 22:45:42 +1100

    rewrote fov library

--- src/Utils/FieldOfView.elm
+++ src/Utils/FieldOfView.elm
@@ -1,32 +1,72 @@
-module Utils.FieldOfView exposing (fov, los)
+module Utils.FieldOfView exposing (find, los)
+
+{-| For any given graph, will span out from the source point, collecting visible nodes
+    until there are no more nodes to collect.
+-}
 
 import Utils.Vector as Vector exposing (Vector)
 import EveryDict exposing (EveryDict)
 import Utils.BresenhamLine as BresenhamLine
+import Set exposing (Set)
 
 
 type alias Map =
     EveryDict Vector Bool
 
 
-fov : Vector -> (Vector -> Bool) -> (Vector -> List Vector) -> List Vector
-fov source isSeeThrough neighbours =
-    fov_ source source isSeeThrough neighbours
+{-| The algorithm starts off with the source node which is unexplored and adds unexplored
+ neighbours that are not part of the explored set. Each time it explores a node, it adds
+ the node to the explored set and if visible, then to the visible set.
+ Once there are no more unexplored nodes, it returns the visible set.
+-}
+type alias FieldOfView comparable =
+    { unexplored : List comparable
+    , explored : Set comparable
+    , visible : Set comparable
+    , isSeeThrough : comparable -> Bool
+    , getNeighbours : comparable -> Set comparable
+    }
 
 
-fov_ : Vector -> Vector -> (Vector -> Bool) -> (Vector -> List Vector) -> List Vector
-fov_ source current isSeeThrough neighbours =
+find : Vector -> (Vector -> Bool) -> (Vector -> Set Vector) -> Set Vector
+find source isSeeThrough neighbours =
     let
-        inSight target =
-            los source target isSeeThrough
-
-        inView revealedPosition =
-            fov_ source revealedPosition isSeeThrough neighbours
+        fov =
+            FieldOfView [ source ] Set.empty Set.empty isSeeThrough neighbours
     in
-        neighbours current
-            |> List.filter inSight
-            |> List.map inView
-            |> List.concat
+        find_ source fov
+            |> .visible
+
+
+find_ : Vector -> FieldOfView Vector -> FieldOfView Vector
+find_ source ({ unexplored, explored, visible, isSeeThrough, getNeighbours } as fov) =
+    let
+        addNeighbours x fov =
+            let
+                neighbours =
+                    getNeighbours x
+            in
+                { fov | unexplored = (Set.diff neighbours fov.explored |> Set.toList) ++ fov.unexplored }
+    in
+        case unexplored of
+            [] ->
+                fov
+
+            x :: xs ->
+                if los source x isSeeThrough then
+                    { fov
+                        | unexplored = xs
+                        , explored = Set.insert x explored
+                        , visible = Set.insert x visible
+                    }
+                        |> addNeighbours x
+                        |> find_ source
+                else
+                    find_ source
+                        { fov
+                            | unexplored = xs
+                            , explored = Set.insert x explored
+                        }
 
 
 los : Vector -> Vector -> (Vector -> Bool) -> Bool
