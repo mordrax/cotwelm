373c1c142eb946b3005d893bd1ed4eb40de8faa9
Joseph Ni <joseph.ni@gmail.com>
2017-05-14 22:55:10 +1000

    refactoring splash screen

--- src/SplashView.elm
+++ src/SplashView.elm
@@ -1,9 +1,9 @@
 module SplashView exposing (view, Msg(..))
 
-import Html exposing (..)
+import Html as H exposing (..)
 import Html.Events as HE
 import Html.Attributes as HA
-import Css
+import Css exposing (..)
 import Game.Render
 
 
@@ -21,53 +21,42 @@ addStyle currentStyles style =
     HA.style (Css.asPairs <| style :: currentStyles)
 
 
-maxWidthHeight : Attribute msg
-maxWidthHeight =
-    styles
-        [ Css.maxWidth (Css.pct 100)
-        , Css.maxHeight (Css.pct 100)
-        , Css.width (Css.pct 100)
-        , Css.height (Css.pct 100)
-        ]
-
-
 {-| Shows the splash screen
 @doc view
 -}
-view : Html msg
+view : Html Msg
 view =
-    Game.Render.viewRip
+    --    Game.Render.viewRip
+    div
+        [ styles
+            [ displayFlex
+            , alignItems center
+            , justifyContent center
+            , flexDirection column
+            , height (pct 100)
+            , backgroundColor (rgb 0 0 0)
+            ]
+        ]
+        [ cotwTitleImg
+        , cotwSubtitleImg
+        , newLoadOverview
+        ]
 
 
-nameView : String -> Html msg
-nameView =
-    text
+cotwTitleImg : Html a
+cotwTitleImg =
+    img [ HA.src "/assets/landing_cotw1.jpg" ] []
 
 
+cotwSubtitleImg : Html a
+cotwSubtitleImg =
+    img [ HA.src "/assets/landing_cotw2.jpg" ] []
 
---view : Html Msg
---view =
---    let
---        bgStyle =
---            [ ( "backgroundColor", "black" ) ]
---    in
---        div
---            [ class "ui center aligned middle aligned grid"
---            , style bgStyle
---            ]
---            [ div [ class "ui one column" ]
---                [ div [ class "ui column" ]
---                    [ img [ src "/assets/landing_cotw1.jpg" ] []
---                    ]
---                , div [ class "ui column" ]
---                    [ img [ src "/assets/landing_cotw2.jpg" ] []
---                    ]
---                , div [ class "ui column" ]
---                    [ div [ class "ui buttons" ]
---                        [ button [ class "ui button primary", onClick NewGame ] [ text "New Game" ]
---                        , button [ class "ui button", onClick LoadGame ] [ text "Load Game" ]
---                        , button [ class "ui button", onClick Overview ] [ text "Overview" ]
---                        ]
---                    ]
---                ]
---            ]
+
+newLoadOverview : Html Msg
+newLoadOverview =
+    div []
+        [ button [ HE.onClick NewGame ] [ H.text "New Game" ]
+        , button [ HE.onClick LoadGame ] [ H.text "Load Game" ]
+        , button [ HE.onClick Overview ] [ H.text "Overview" ]
+        ]
